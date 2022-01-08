82deee3f42c374aacbf2ab9eb8b908adef3b0a79
Mohammad Mater <moe.matar1998@gmail.com>
2022-01-08 19:45:40 +0100

    step2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-node_modules;
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    //get information bijib cha8let /nmare2la route */* ma3 call back function
+    res.send("ok");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`); // call back function
+});
